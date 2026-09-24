28f2d35da023d9fdf1d92c0621c3a945672cad6f
Samir-Macias <tu_email_de_github@ejemplo.com>
2026-09-24 19:39:54 +0200

    reto 1 cumplido

---
+++ Semana_01/Reto_0/src/main/java/org/example/Episodio.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Episodio {
+    private String titulo;
+    private int  duracion;
+
+
+    //Contructores
+
+    public Episodio (String titulo, int duracion){
+        this.titulo = titulo;
+        this.duracion = duracion;
+
+    }
+
+    //Metodos
+    public void procesar(){
+        System.out.println("Iniciando procesamineto de : " + this.titulo + "...");
+        try {
+            Thread.sleep(2000);
+
+        }catch (InterruptedException e){
+            System.out.println("Error al procesar el episodio");
+        }
+        System.out.println("Completado: " + this.titulo);
+    }
+
+}
--- Semana_01/Reto_0/src/main/java/org/example/Main.java
+++ Semana_01/Reto_0/src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
+
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.println("--- INICIANDO UDITVERSUM");
+        ArrayList<Episodio> catalogo = new ArrayList<>();
+        catalogo.add(new Episodio("Diseño 3d - Intro", 45));
+        catalogo.add(new Episodio("Animacion", 60));
+        catalogo.add(new Episodio("Texturas", 50));
+
+        long inicio = System.currentTimeMillis();
+
+        for (Episodio ep: catalogo){
+            ep.procesar();
         }
+
+        long fin = System.currentTimeMillis();
+
+        long totalSegundos = (fin - inicio) / 1000;
+        System.out.print("Tiempo total: " + totalSegundos + " Segundos de bloqueo. ");
+
     }
 }
